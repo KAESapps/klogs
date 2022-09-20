c3a710e500d954fd5c1cf5f89ba9e9e371e3654c
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2022-09-20 13:44:21 +0200

    better console log in vscode

--- writers/console.js
+++ writers/console.js
@@ -1,12 +1,14 @@
 const { styledLevels, coloredByName } = require("./utils");
 
-module.exports = ({ name, diff, level, data }) => {
+module.exports = ({ name, diff, level, data, time }) => {
   const colored = coloredByName(name);
   console[level](
-    // new Date(time).toISOString(),
     styledLevels[level],
     colored(`[${name}]`),
-    ...data,
+    // ...data,
+    colored(new Date(time).toISOString()),
     colored(`+${diff}ms`)
   );
+  // log des données sur une 2ème ligne car la console vscode n'arrive pas à gérer la couleur quand il y a des objets
+  console[level](...data);
 };
